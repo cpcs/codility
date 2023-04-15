8e053ac813809ad1ace12c51ff22ae515c243a60
Peng Cao <cao_peng@126.com>
2023-04-15 01:32:59 -0700

    Update solution.cpp

--- challenges/Germanium 2018/solution.cpp
+++ challenges/Germanium 2018/solution.cpp
@@ -1,19 +1,61 @@
-You are playing a game with N cards. On both sides of each card there is a positive integer. The cards are laid on the table. The score of the game is the smallest positive integer that does not occur on the face-up cards. You may flip some cards over. Having flipped them, you then read the numbers facing up and recalculate the score. What is the maximum score you can achieve?
+// you can use includes, for example:
+// #include <algorithm>
 
-Write a function:
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
 
-int solution(vector<int> &A, vector<int> &B);
+#include <bits/stdc++.h>
+using namespace std;
 
-that, given two arrays of integers A and B, both of length N, describing the numbers written on both sides of the cards, facing up and down respectively, returns the maximum possible score.
+vector<int> dfs(const vector<vector<int>> &con, int x, vector<bool> &mark) {
+    // Return number of nodes, sum of degrees.
+    vector<int> r(2);
+    if (mark[x]) {
+        return r;
+    }
+    mark[x] = true;
+    r = {1, con[x].size()};
+    for (int y : con[x]) {
+        const auto& v = dfs(con, y, mark);
+        r[0] += v[0];
+        r[1] += v[1];
+    }
+    return r;
+}
 
-For example, given A = [1, 2, 4, 3] and B = [1, 3, 2, 3], your function should return 5, as without flipping any card the smallest positive integer excluded from this sequence is 5.
-
-Given A = [4, 2, 1, 6, 5] and B = [3, 2, 1, 7, 7], your function should return 4, as we could flip the first card so that the numbers facing up are [3, 2, 1, 6, 5] and it is impossible to have both numbers 3 and 4 facing up.
-
-Given A = [2, 3] and B = [2, 3] your function should return 1, as no matter how the cards are flipped, the numbers facing up are [2, 3].
-
-Write an efficient algorithm for the following assumptions:
-
-N is an integer within the range [1..100,000];
-each element of arrays A and B is an integer within the range [1..100,000,000];
-input arrays are of equal size.
+int solution(vector<int> &A, vector<int> &B) {
+    // Implement your solution here
+    const int n = A.size();
+    vector<bool> reachable(n);
+    vector<vector<int>> con(n);
+    unordered_set<int> s;
+    for (int i = 0; i < n; ++i) {
+        const int x = max(A[i], B[i]) - 1, y = min(A[i], B[i]) - 1;
+        if (y >= n) {
+            continue;
+        }
+        reachable[y] = true;
+        if (x > y && x < n) {
+            con[y].push_back(x);
+            con[x].push_back(y);
+            reachable[x] = true;
+        } else {
+            con[y].push_back(y);
+        }
+    }
+    vector<bool> mark(n);
+    int r = n;
+    for (int i = n - 1; i >= 0; --i) {
+        if (!reachable[i]) {
+            r = i;
+        } else if (!mark[i]) {
+            const auto& v = dfs(con, i, mark);
+            // If it's a tree, make the largest number (which is i) unreachable.
+            if (((v[0] - 1) << 1) == v[1]) {
+                // reachable[i] = false;
+                r = i;
+            }
+        }
+    }
+    return r + 1;
+}
