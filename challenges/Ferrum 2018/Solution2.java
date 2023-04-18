c9477af30c8652f8dc12f95bc1beec6a4f8ffcac
Peng Cao <cao_peng@126.com>
2023-04-17 17:46:00 -0700

    Update Solution2.java

--- challenges/Ferrum 2018/Solution2.java
+++ challenges/Ferrum 2018/Solution2.java
@@ -12,20 +12,16 @@ class Solution {
         int[] p = new int[n + 1];
         // 以这一项结尾的子数组的最大和
         int[] best = new int[n];
-        int len = 0;
+        int length = 0;
         for (int i = 1, m = 0; i <= n; ++i) {
             p[i] = p[i - 1] + A[i - 1];
             best[i - 1] = p[i] - p[m];
-            int maybe = len;
-            for (; maybe < i && best[i - maybe - 1] + p[i] - p[i - maybe] >= 0; ++maybe)
+            for (; length < i && best[i - length - 1] + p[i] - p[i - length] >= 0; ++length)
             ;
-            if (p[i] - p[i - maybe] >= 0) {
-                len = maybe;
-            }
             if (p[i] < p[m]) {
                 m = i;
             }
         }
-        return len;
+        return length;
     }
 }
