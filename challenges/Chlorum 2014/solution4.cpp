e583d29a55a1dd7e74e2559b42c23189cd9925b5
Peng Cao <cao_peng@126.com>
2023-04-25 23:21:19 -0700

    Create solution4.cpp

---
+++ challenges/Chlorum 2014/solution4.cpp
@@ -0,0 +1,83 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int dfs(const vector<unordered_set<int>> &con, const vector<int> &D, vector<bool> &mark, int x, int f, int w) {
+    if (D[x] < w || mark[x]) {
+        return 0;
+    }
+    mark[x] = true;
+    int r = 1;
+    for (int y : con[x]) {
+        if (y != f) {
+            r += dfs(con, D, mark, y, x, w);
+        }
+    }
+    return r;
+}
+
+void dfs(const vector<unordered_set<int>> &con, const vector<int> &D, vector<int> &mini, int x, int f, int w) {
+    mini[x] = w;
+    w = min(w, D[x]);
+    for (int y : con[x]) {
+        if (y != f) {
+            dfs(con, D, mini, y, x, w);
+        }
+    }
+}
+
+int solution(int K, vector<int> &C, vector<int> &D) {
+    // Implement your solution here
+    const int n = C.size();
+    if (K == 1 || K == n) {
+        return K;
+    }
+    vector<unordered_set<int>> con(n);
+    int m = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i != C[i]) {
+            con[i].insert(C[i]);
+            con[C[i]].insert(i);
+        }
+        if (D[m] < D[i]) {
+            m = i;
+        }
+    }
+    vector<bool> mark(n);
+    int r = 0;
+    for (int i = 0; i < n; ++i) {
+        r = max(r, dfs(con, D, mark, i, -1, D[m]));
+    }
+    r = min(r, K);
+    if (r == K) {
+        return K;
+    }
+    vector<int> mini(n);
+    dfs(con, D, mini, m, -1, INT_MAX);
+    set<vector<int>> leaves;
+    multiset<int> remain;
+    for (int i = 0; i < n; ++i) {
+        if (con[i].size() == 1) {
+            leaves.insert({D[i], mini[i] < D[i] ? 0 : 1, i});
+        }
+        remain.insert(D[i]);
+    }
+    for (int v = INT_MIN; remain.size() > K || *remain.begin() < v;) {
+        const int leaf = (*leaves.begin())[2];
+        v = max(v, D[leaf]);
+        remain.erase(remain.find(D[leaf]));
+        leaves.erase(leaves.begin());
+        const int other = *con[leaf].begin();
+        con[other].erase(leaf);
+        if (con[other].size() == 1) {
+            leaves.insert({D[other], mini[other] < D[other] ? 0 : 1, other});
+        }
+    }
+    r = max(r, (int) remain.size());
+    return r;
+}
